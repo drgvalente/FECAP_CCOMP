5695aa482e2d12a6a3bb8a2224c6b90e4ba1ead1
Adriano FV <54666006+drgvalente@users.noreply.github.com>
2025-04-14 11:12:54 -0300

    Aula 14-04
    Criamos script da Bullet
    Criamos Enemy e script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,14 +4,26 @@ public class Bullet : MonoBehaviour
 {
     float speed = 10f;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject, 5f); // destrói a própria bullet após 5 segundos
     }
 
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            Debug.Log("Acertou Enemy");
+            Enemy en = col.GetComponent<Enemy>();
+            en.TakeDamage();
+        }
+        Destroy(gameObject);
     }
 }
